7f8636e029f08068c11e4fa3f713acc6c69dd292
Jon Gaul <jon@gaul.tech>
2023-09-28 13:15:53 +0100

    Fix deployment path (api)

--- api/src/main/java/tech/gaul/noughtsncrosses/web/api/NoughtsncrossesApiApplication.java
+++ api/src/main/java/tech/gaul/noughtsncrosses/web/api/NoughtsncrossesApiApplication.java
@@ -2,9 +2,10 @@ package tech.gaul.noughtsncrosses.web.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class NoughtsncrossesApiApplication {
+public class NoughtsncrossesApiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.
